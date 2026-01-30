475b846f3f88c09f143d88b4c9bb1d7c0919fd29
Mazharul Islam Leon <xtleon.xl@gmail.com>
2026-01-30 22:56:54 +0600

    feat: setup FastAPI, pytest, uv, and GitHub Actions CI workflow

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-def hello_world():
-    return "Hello World"
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello World"}
+
 
 if __name__ == "__main__":
-    print(hello_world())
+    uvicorn.run(app, host="127.0.0.1", port=8000)
